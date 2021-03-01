2f6daf821bb5cc8b8d36850c72ea8db0faf28e97
Chris <cquan808@gmail.com>
2021-03-01 15:46:10 -0700

    Add files

---
+++ main.py
@@ -0,0 +1,32 @@
+# This is a simple application to convert your weight from lbs to kg or
+# from kg to lbs
+
+import unitConverter
+
+
+class PrintResults():
+    def print_results(name, weight, unit_result):
+        print(f"Hello {name}, your weight is {weight} in {unit_result}.")
+
+
+name = input("What is your name: ")
+try:
+    weight = int(input("What is your weight: "))
+except ValueError:
+    print("Invalid value")
+
+unit = (input("(K)g or (l)bs: ")).upper()
+unit_result = ""
+
+if unit == "K":
+    unit_result = "lbs"
+    weight = unitConverter.kg_to_lbs(weight)
+    # print(f"Hello {name}, your weight is {weight} in {unit_result}")
+    PrintResults.print_results(name, weight, unit_result)
+elif unit == "L":
+    unit_result = "kg"
+    weight = unitConverter.lbs_to_kg(weight)
+    # print(f"Hello {name}, your weight is {weight} in {unit_result}")
+    PrintResults.print_results(name, weight, unit_result)
+else:
+    print("Unit must be 'k' for kg or 'l' for lbs, please try again")
