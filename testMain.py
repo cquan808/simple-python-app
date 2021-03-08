a37a25faababf31530fe0b905d6c43989e175065
Chris <cquan808@gmail.com>
2021-03-08 13:03:49 -0700

    Create test file
    test functions

---
+++ testMain.py
@@ -0,0 +1,23 @@
+import unittest
+import main
+import unitConverter
+
+
+class TestMain(unittest.TestCase):
+    """
+    Test the add function from the calc library
+    """
+
+    def test_kg_to_lbs(self):
+        """
+        Test that kg is converted to lbs
+        """
+        result = unitConverter.kg_to_lbs(145)
+        self.assertEqual(result, 65.25)
+
+    def test_lbs_to_kg(self):
+        """
+        Test that lbs is converted to kg
+        """
+        result = unitConverter.kg_to_lbs(90)
+        self.assertEqual(result, 200.0)
